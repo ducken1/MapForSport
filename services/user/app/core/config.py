e070aa3c778a862c24781f38237d31b69f6fbfe0
ducken1 <luka.lasic123@gmail.com>
2025-03-23 22:11:07 +0100

    a

--- services/user/app/core/config.py
+++ services/user/app/core/config.py
@@ -1,9 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+JWT_SECRET = os.getenv("JWT_SECRET")
+import os
 
 db_host = os.getenv("MYSQL_HOST", "localhost")  # default to localhost if not set
 db_user = os.getenv("MYSQL_USER", "root")
 db_password = os.getenv("MYSQL_PASSWORD", "Geslo123")
-db_name = os.getenv("MYSQL_DATABASE", "testdb")
+db_name = os.getenv("MYSQL_DATABASE", "user")
 
 # Build the connection URL
 DATABASE_URL = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}"
