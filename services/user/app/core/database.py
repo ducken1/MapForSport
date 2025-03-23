6b0fd872a4d664d17350da3c02464226646804e2
ducken1 <luka.lasic123@gmail.com>
2025-03-23 23:45:53 +0100

    gh

--- services/user/app/core/database.py
+++ services/user/app/core/database.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import DATABASE_URL
 from app.core.logger import logger
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})  # SQLite requires this for multi-threaded access
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
 def init_db():
